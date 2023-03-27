041bce76fb27b1e3e4f6d72f02d3a7ab572f85ca
Abel <se.abel.tadesse@gmail.com>
2023-03-27 14:29:47 +0300

    Icon

--- App.js
+++ App.js
@@ -1,23 +1,11 @@
-import {
-  useDeviceOrientation,
-  useDimensions,
-} from "@react-native-community/hooks";
-import { StatusBar } from "expo-status-bar";
-import {
-  SafeAreaView,
-  Platform,
-  StyleSheet,
-  View,
-  Text,
-  Button,
-  Image,
-  TouchableWithoutFeedback,
-  Alert,
-  ImageBackground,
-} from "react-native";
+import { View } from "react-native";
 import WelcomeScreen from "./app/screen/WelcomeScreen";
 import ViewImageScreen from "./app/screen/ViewImageScreen";
-
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 export default function App() {
-  return <ViewImageScreen />;
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <MaterialCommunityIcons name="email" size={100} color="red" />
+    </View>
+  );
 }
