fd7d37b2967ecab945c199ee7f0b3fe9bf002633
Abel <se.abel.tadesse@gmail.com>
2023-03-27 13:51:45 +0300

    welcome screen done

---
+++ app/screen/ViewImageScreen.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Image } from "react-native";
+
+function ViewImageScreen(props) {
+  return <Image source={require("../assets/aripod.jpg")} />;
+}
+
+export default ViewImageScreen;
