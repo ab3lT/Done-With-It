aab24cb56c61b16d3807ff6d5dfd91d9e6ea160f
Abel <se.abel.tadesse@gmail.com>
2023-03-27 14:49:04 +0300

    Text Styling

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
-import { View } from "react-native";
+import { View, Text } from "react-native";
 import WelcomeScreen from "./app/screen/WelcomeScreen";
 import ViewImageScreen from "./app/screen/ViewImageScreen";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 export default function App() {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <MaterialCommunityIcons name="email" size={100} color="red" />
-      <View
+      <Text
         style={{
-          backgroundColor: "dodgerblue",
-          width: 100,
-          height: 100,
-          shadowOpacity: 1,
-          elevation: 50,
+          fontSize: 30,
+          fontFamily: "Roboto",
+          fontStyle: "italic",
+          fontWeight: "600",
+          color: "tomato",
+          textTransform: "capitalize",
+          textDecorationLine: "underline",
+          textAlign: "center",
+          lineHeight: 30,
         }}
-      ></View>
+      >
+        I Love React Native! This is Not My First React Native App! Here's Some
+        more text
+      </Text>
     </View>
   );
 }
