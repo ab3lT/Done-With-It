3a51b4b934ee592f6092c9f223c9b2cced5ca78e
Abel <se.abel.tadesse@gmail.com>
2023-04-04 18:47:27 +0300

    Pull To Refersh

--- app/screen/MessagesScreen.js
+++ app/screen/MessagesScreen.js
@@ -1,6 +1,7 @@
 import { StyleSheet, View, FlatList } from "react-native";
 import React, { useState } from "react";
 import ListItem from "../components/ListItem";
+import Screen from "../components/Screen";
 import ListItemSepartor from "../components/ListItemSepartor";
 
 const initialMessages = [
@@ -20,32 +21,44 @@ const initialMessages = [
 
 export default function MessagesScreen() {
   const [messages, setmessages] = useState(initialMessages);
+  const [refreshing, setRefreshing] = useState(false);
   const handleDelete = (message) => {
     // Delete the message from messages
     // delete the message from server
     setmessages(messages.filter((m) => m.id !== message.id));
   };
   return (
-    <FlatList
-      data={messages}
-      keyExtractor={(messages) => messages.id.toString()}
-      renderItem={({ item }) => (
-        <ListItem
-          title={item.title}
-          subTitle={item.description}
-          image={item.image}
-          onPress={() => console.log("Message selected", item)}
-          renderRightActions={() => {
-            () => (
+    <Screen>
+      <FlatList
+        data={messages}
+        keyExtractor={(messages) => messages.id.toString()}
+        renderItem={({ item }) => (
+          <ListItem
+            title={item.title}
+            subTitle={item.description}
+            image={item.image}
+            onPress={() => console.log("Message selected", item)}
+            renderRightActions={() => (
               <ListItemDeleteAction
                 onPress={() => handleDelete(item)}
               ></ListItemDeleteAction>
-            );
-          }}
-        />
-      )}
-      ItemSeparatorComponent={ListItemSepartor}
-    />
+            )}
+          />
+        )}
+        ItemSeparatorComponent={ListItemSepartor}
+        refreshing={refreshing}
+        onRefresh={() => {
+          setmessages([
+            {
+              id: 2,
+              title: "T2",
+              description: "D2",
+              image: require("../assets/apple_product.jpg"),
+            },
+          ]);
+        }}
+      />
+    </Screen>
   );
 }
 
